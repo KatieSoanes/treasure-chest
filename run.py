8e443bf9411051bdad48ca351bd75e82a84de219
KatieSoanes <katiesoanes@hotmail.com>
2021-09-15 06:34:08 +0000

    Add placeholder text

--- run.py
+++ run.py
@@ -1,4 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
-print("hello")
+#Round 1
+def intro():
+    print()
+
+# Round 2
+
+# Round 3
+
+# Round 4
+
+# Mini games
+# Guess the riddle
+# Rock, Paper, Scissors
+
+def main():
