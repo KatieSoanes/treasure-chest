7082255bd06b298a12bd29be16235353cdec531b
KatieSoanes <katiesoanes@hotmail.com>
2021-09-15 18:55:32 +0000

    Add round 1 of choice for user

--- run.py
+++ run.py
@@ -1,6 +1,26 @@
 #Round 1
 def intro():
-    print("HIII")
+        print("You are now on top of Skull island mountain")
+        print("You need to get to the base")
+        print("Which option do you take?")
+        print("""Option #1: Climb down the mountain face by rope,
+                be careful, the rope appears old and brittle""")
+        print("""Option #2: Make a raft and descend down by Mountain River, but you don't
+                know how shallow and rocky it is"
+                """)
+        print("""Option #3: Walk by foot, the slowest option
+                and you don't know what challenges you could face""")
+        round_one = input("Which will you you choose? (1, 2 or 3)")
+        if round_one == "1":
+                print()
+                option1()
+        elif round_one == "2":
+                print()
+                option2()
+        elif round_one == "3":
+                print()
+                option3()
+
 
 # Round 2
 
@@ -37,14 +57,14 @@ def main():
         print("$ The treasure contains gold, silver and jewels....")
         print("worth Millions!!")
         print("$ The map leads us to Skull island which is known")
-        print("$ to be a formidable dangerous place")
+        print("$ to be a formidable dangerous place,")
         print("$ where not everyone returns...")
         print("So, do you have whay it takes to find the treasure?")
         startGame = input("$ Yes/No: ").lower()
         if startGame == "no":
                 print("$ Oh no! Are you sure?")
                 print("$ Last chance to play....")
-                startGame = input("$ type yes").lower
+                startGame = input("$ type yes:  ").lower
                 startGame == "yes"
                 intro()
         elif startGame == "yes":
