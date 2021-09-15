3e05b2b24b5a3f8bd73f6b6a075c2165d6496c19
KatieSoanes <katiesoanes@hotmail.com>
2021-09-15 07:32:41 +0000

    Add main() function

--- run.py
+++ run.py
@@ -12,38 +12,42 @@ def intro():
 # Guess the riddle
 # Rock, Paper, Scissors
 
-#def main():
+
 # OPENING INTRODUCTORY TEXT firstttt
 # Asking user if they would like to play the game
-print(" ")
-print(" ")
-print("     **********************")
-print("     *        ***         *")
-print("     *  Treasure Chest    *")
-print("     *  by Katie Soanes   *")
-print("     **********************")
-print(" ")
-print(" ")
-name = input("Enter your name ")
-print("$ Hello " + name + " welcome to TREASURE CHEST")
-print("$ Treasure chest is an interactive adventure game!")
-print("$ The game where YOU get to choose your own destiny...")
-print("$ To play the game you must type in your answer")
-print("$ followed by the enter key on your keyboard.")
-print("$ Before we begin, let me tell you a story...")
-print("$ Sometime long ago I stumbled upon a treasure map,")
-print("$ belonging to my great-great-grandfather...")
-print("$ The treasure contains gold, silver and jewels worth Millions!!")
-print("$ The map leads us to Skull island which is known")
-print("$ to be a formidable dangerous place")
-print("$ where not everyone returns...")
-print("So, do you have whay it takes to find the treasure?")
-startGame = input(" Yes/No: ").lower()
-if startGame == "no":
-        print("Oh no! Are you sure?")
-        print("Last chance to play....")
-        startGame = input("type yes").lower
-        startGame == "yes"
-        intro()
-elif startGame == "yes":
-        intro()
+def main():
+        print(" ")
+        print(" ")
+        print("     **********************")
+        print("     *        ***         *")
+        print("     *  Treasure Chest    *")
+        print("     *  by Katie Soanes   *")
+        print("     **********************")
+        print(" ")
+        print(" ")
+        name = input("Enter your name ")
+        print("$ Hello " + name + " welcome to TREASURE CHEST")
+        print("$ Treasure chest is an interactive adventure game!")
+        print("$ The game where YOU get to choose your own destiny...")
+        print("$ To play the game you must type in your answer")
+        print("$ followed by the enter key on your keyboard.")
+        print("$ Before we begin, let me tell you a story...")
+        print("$ Sometime long ago I stumbled upon a treasure map,")
+        print("$ belonging to my great-great-grandfather...")
+        print("$ The treasure contains gold, silver and jewels....")
+        print("worth Millions!!")
+        print("$ The map leads us to Skull island which is known")
+        print("$ to be a formidable dangerous place")
+        print("$ where not everyone returns...")
+        print("So, do you have whay it takes to find the treasure?")
+        startGame = input(" Yes/No: ").lower()
+        if startGame == "no":
+                print("Oh no! Are you sure?")
+                print("Last chance to play....")
+                startGame = input("type yes").lower
+                startGame == "yes"
+                intro()
+        elif startGame == "yes":
+                intro()
+
+main()
